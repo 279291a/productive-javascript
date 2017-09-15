d15d4af215a995a3c0e6dcb3616877a46e44dd98
王晓丽 <xiaoli.wang@echoesnet.com>
2017-09-15 15:03:17 +0800

    无需使用new 调用的构造函数的两种写法

--- Object/prototype.js
+++ Object/prototype.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 // proototype、__proto__和getprototypeOf 的不同
 function User(name, passwordHash) {
   this.name = name;
@@ -20,3 +21,50 @@ u.__proto__ === User.prototype;
 const empty = Object.create(null);
 '__proto__' in empty; // false
 
+//使用构造函数与new 操作符无关
+
+//①
+function User1(name, passwordHash) {
+  if(!(this instanceof User1)){
+    return new User1(name,passwordHash);
+  }
+
+  this.name = name;
+  this.passwordHash = passwordHash;
+}
+
+const x = User1('w','xl');
+const y = new User1('w','xl');
+
+console.log(x instanceof User1);
+console.log(y instanceof User1);function User1(name, passwordHash) {
+  if(!(this instanceof User1)){
+    return new User1(name,passwordHash);
+  }
+
+  this.name = name;
+  this.passwordHash = passwordHash;
+}
+
+const x = User1('w','xl');
+const y = new User1('w','xl');
+
+console.log(x instanceof User1);
+console.log(y instanceof User1);
+
+//②
+function User2(name,passwordHash){
+  var self = this instanceof User2
+    ? this
+    : Object.create(User2.prototype);
+  self.name = name;
+  self.passwordHash = passwordHash;
+
+  return self;
+}
+
+const a = User2('w','xl');
+const b = new User2('w','xl');
+
+console.log(a instanceof User2);
+console.log(b instanceof User2);
