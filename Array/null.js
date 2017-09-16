53c82d040b4e21fd76db31ddab4137733432d73d
王晓丽 <xiaoli.wang@echoesnet.com>
2017-09-16 13:44:25 +0800

    Object.create()函数能够使用一个指定原型链动态构造对象

--- Array/null.js
+++ Array/null.js
@@ -3,5 +3,8 @@ function C() {}
 C.prototype = null;
 
 const o = new C();
-Object.getPrototypeOf(o) === null;
-Object.getPrototypeOf(o) === Object.prototype;
+Object.getPrototypeOf(o) === null;  //false
+Object.getPrototypeOf(o) === Object.prototype;   //true
+
+var x = Object.create(null);
+Object.getPrototypeOf(x) === null;  //true
