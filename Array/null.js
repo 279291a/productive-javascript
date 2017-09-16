e0d668329c4804dd1ed347e49dabb53630727ead
王晓丽 <xiaoli.wang@echoesnet.com>
2017-09-16 13:50:56 +0800

    使用__proto__创建原型为null 的对象

--- Array/null.js
+++ Array/null.js
@@ -8,3 +8,6 @@ Object.getPrototypeOf(o) === Object.prototype;   //true
 
 var x = Object.create(null);
 Object.getPrototypeOf(x) === null;  //true
+
+var y = {__proto__:null};
+y instanceof Object;  //false
