efb57ac1524a7c65af3a017d596022d9a3305249
王晓丽 <xiaoli.wang@echoesnet.com>
2017-09-21 13:41:50 +0800

    自定义自己的迭代抽象

--- Array/array.js
+++ Array/array.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/*eslint-disable*/
 const dict = { alice: 34, bob: '24', chris: 62 };
 const people = [];
 
@@ -8,48 +8,60 @@ for (const name in dict) {
 
 console.log(people);
 
-function NaiveDict(){}
+function NaiveDict() {}
 
 NaiveDict.prototype.count = () => {
   let i = 0;
-  for(var name in this){
+  for (const name in this) {
     i++;
   }
   return i;
-}
+};
 
-NaiveDict.prototype.toString = () => {
-  return '[object NaiveDict]'
-}
+NaiveDict.prototype.toString = () => '[object NaiveDict]';
 
 const dict2 = new NaiveDict();
 
 dict2.alice = 34;
 dict2.bob = 24;
 dict2.chris = 62;
-console.log(dict2)
-console.log(dict2.count()); //287。。。。。。
+console.log(dict2);
+console.log(dict2.count()); // 287。。。。。。
 
 // var dict3= new Array();
-var dict3= {};
+const dict3 = {};
 
 dict3.alice = 34;
 dict3.bob = 24;
 dict3.chris = 62;
 
-Array.prototype.first = function(){
+Array.prototype.first = function () {
   return this[0];
-}
+};
 
-Array.prototype.last = function(){
-  return this[this.length -1];
-}
+Array.prototype.last = function () {
+  return this[this.length - 1];
+};
 
 // var names = [];
-var names = [];
-for(var name in dict3){
+const names = [];
+for (const name in dict3) {
   names.push(name);
 }
 
-console.log(names)
+console.log(names);
+
+// 自定义自己的迭代抽象
+
+function takeWhile(a, pred) {
+  const result = [];
+  const n = a.length;
+  for (let i = 0; i < n; i++) {
+    if (!pred(a[i], i)) {
+      break;
+    }
+    result[i] = a[i];
+  }
+  return result;
+}
 
